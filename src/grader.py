18a9558b19f3fb95c1840473abc6bbcebca701b3
ledelma2 <32878257+ledelma2@users.noreply.github.com>
2018-04-18 14:52:35 -0500

    Added Word Counter

--- src/grader.py
+++ src/grader.py
@@ -5,3 +5,15 @@ Created on Wed Apr 18 12:43:33 2018
 @author: Liam Edelman
 """
 
+import nltk
+import string
+
+EssayFile = open("test.txt", "r")
+essay = EssayFile.read()
+
+indices = []
+nopunc = ''.join(word for word in essay if word not in string.punctuation)
+
+tokens = nltk.word_tokenize(nopunc)
+  
+print(len(tokens))
