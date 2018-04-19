6d3b1fd7acbb9185b40c0a1a4782385293597a64
chabel2 <32532999+chabel2@users.noreply.github.com>
2018-04-18 19:46:24 -0500

    Added word count scoring
    also loops through files

--- src/grader.py
+++ src/grader.py
@@ -7,13 +7,51 @@ Created on Wed Apr 18 12:43:33 2018
 
 import nltk
 import string
+import os
 
-EssayFile = open("test.txt", "r")
-essay = EssayFile.read()
+results = open("../output/results.txt", "w")
 
-indices = []
-nopunc = ''.join(word for word in essay if word not in string.punctuation)
+# Store every file in the input folder
+files = os.listdir("../input/essays/")
 
-tokens = nltk.word_tokenize(nopunc)
+for file in files:
+	essay = open("../input/essays/" + file, "r").read()
+
+	indices = []
+	nopunc = ''.join(word for word in essay if word not in string.punctuation)
+	tokens = nltk.word_tokenize(nopunc)
   
-print(len(tokens))
+	
+
+	# Word length score
+	length_score = 0
+	word_count = len(tokens)
+
+	if word_count  < 100:
+		length_score = 0
+
+	elif word_count < 150:
+		length_score = 1
+
+	elif word_count < 200:
+		length_score = 2
+
+	elif word_count < 250:
+		length_score = 3
+
+	elif word_count < 300:
+		length_score = 4
+
+	else:
+		length_score = 5
+
+	results.write(file + ";")
+	results.write(str(length_score) + ";\n")
+
+
+
+
+
+
+
+results.close()	
