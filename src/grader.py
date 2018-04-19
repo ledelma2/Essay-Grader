89e3f11dc2cf73e18371a14b31e398c7027c8159
ledelma2 <32878257+ledelma2@users.noreply.github.com>
2018-04-18 22:38:34 -0500

    Added SpellChecking

--- src/grader.py
+++ src/grader.py
@@ -8,11 +8,37 @@ Created on Wed Apr 18 12:43:33 2018
 import nltk
 import re
 import os
-
-EssayFile = open("test.txt", "r")
-essay = EssayFile.read()
+from bisect import bisect_left
 
 punc = "!\"#$%&()*+,-./:;<=>?@[\]^_`{|}~"
+
+dictionary = open("../src/resources/dictionary.txt", "r")
+dic = dictionary.read()
+dictionary.close()
+dictionary = re.split("\n", dic)
+dictionary.append("the")
+dictionary.append("of")
+dictionary.append("to")
+dictionary.append("my")
+dictionary.append("for")
+dictionary.append("if")
+dictionary.append("If")
+dictionary.append("You")
+dictionary.append("When")
+dictionary.append("We")
+dictionary.sort()
+
+
+# -*- coding: utf-8 -*-
+
+def binary_search(a, x, lo=0, hi=None):
+    hi = hi or len(a)
+    pos = bisect_left(a, x, lo, hi)
+    return (pos if pos != hi and a[pos] == x else -1)
+
+
+test = open("test.txt", "r")
+essay = test.read()
 nopunc = ''.join(word for word in essay if word not in punc)
 tokens = re.split(" |\n|\t", nopunc)
 indices = []
@@ -22,63 +48,73 @@ for i in range (0, len(tokens)):
 
 for i in range (0, len(indices)):
     del tokens[indices[i]]
-
+    
 print(len(tokens))
-  
+
+
 mistakes = 0
 
 for words in tokens:
-    array = len(nltk.corpus.wordnet.synsets(words))
-    if array < 1:
-        print(words)
-        mistakes = mistakes + 1
+        exists = binary_search(dictionary, words)
+        array = len(nltk.corpus.wordnet.synsets(words))
+        if exists == -1 and array < 1:
+            print(words)
+            mistakes = mistakes + 1
+        
 
-
-# -*- coding: utf-8 -*-
-
-results = open("../output/results.txt", "w")
-
-# Store every file in the input folder
-files = os.listdir("../input/essays/")
-
-for file in files:
-	essay = open("../input/essays/" + file, "r").read()
-
-	indices = []
-	nopunc = ''.join(word for word in essay if word not in punc)
-	tokens = nltk.word_tokenize(nopunc)
-  
-	
-
-	# Word length score
-	length_score = 0
-	word_count = len(tokens)
-
-	if word_count  < 100:
-		length_score = 0
-
-	elif word_count < 150:
-		length_score = 1
-
-	elif word_count < 200:
-		length_score = 2
-
-	elif word_count < 250:
-		length_score = 3
-
-	elif word_count < 300:
-		length_score = 4
-
-	else:
-		length_score = 5
-
-	results.write(file + ";")
-	results.write(str(length_score) + ";\n")
-
-
-
-
-
-
-
-results.close()	
+#results = open("../output/results.txt", "w")
+#
+## Store every file in the input folder
+#files = os.listdir("../input/essays/")
+#
+#for file in files:
+#    mistakes = 0
+#    essay = open("../input/essays/" + file, "r").read()
+#    
+#    nopunc = ''.join(word for word in essay if word not in punc)
+#    tokens = re.split(" |\n|\t", nopunc)
+#    indices = []
+#    for i in range (0, len(tokens)):
+#        if len(tokens[i]) == 0:
+#            indices.insert(0, i)
+#    
+#    for i in range (0, len(indices)):
+#        del tokens[indices[i]]
+#    	
+#    
+#    # Word length score
+#    length_score = 0
+#    word_count = len(tokens)
+#    
+#    if word_count  < 100:
+#        length_score = 0
+#    
+#    elif word_count < 150:
+#        length_score = 1
+#    
+#    elif word_count < 200:
+#        length_score = 2
+#    
+#    elif word_count < 250:
+#        length_score = 3
+#    
+#    elif word_count < 300:
+#        length_score = 4
+#    
+#    else:
+#        length_score = 5
+#    
+#    results.write(file + ";")
+#    results.write(str(length_score) + ";\n")
+#
+#
+#    for words in tokens:
+#        exists = binary_search(dictionary, words)
+#        if exists < -1:
+#            print(words)
+#            mistakes = mistakes + 1
+#
+#
+#
+#
+#results.close()	
